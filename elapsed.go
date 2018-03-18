729ecef7ef746fbf64dff9757a2f43f6fb005f14
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2018-03-18 14:51:05 +0900

    add ElapsedMillisecond as a shortcut func

--- elapsed.go
+++ elapsed.go
@@ -16,6 +16,10 @@ func (t timer) Elapsed() time.Duration {
 	return time.Since(t.start)
 }
 
+func (t timer) ElapsedMilliseconds() int64 {
+	return time.Since(t.start).Nanoseconds() / int64(time.Millisecond)
+}
+
 func (t *timer) Reset() {
 	t.start = time.Now()
 }
